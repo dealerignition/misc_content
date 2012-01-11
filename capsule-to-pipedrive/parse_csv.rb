fe66696dccf20e149b05e0bc29abe9a10bbbb64a
Luke Seelenbinder <luke.seelenbinder@gmail.com>
2012-01-10 19:43:04 -0500

    [#23010491] Add parsing code for orginizations.
    orgs.csv: list of orginizations in Pipedrive with ids and emails
    matches.csv: list of orginizations in contacts.csv matched with Pipedrive
    unmatched.txt: list of orginizations in contacts.csv not matched with Pipedrive
    
    bi: A Ruby gem for binary searches
    
    parse_csv.rb: Ruby code to handle parsing of contacts.csv
    
    The code to parse and match orginizations with Pipedrive is written to do the following:
      1. Check for identical match with a binary search.
      2. Check for a 90% match in the 5 closest locations by correct alphabetical position.
      3. Check for a 90% match in the entire list.
      4. Fail if the above 3 fail.

--- capsule-to-pipedrive/parse_csv.rb
+++ capsule-to-pipedrive/parse_csv.rb
@@ -1,57 +1,125 @@
 #!/usr/bin/ruby
 require 'csv'
+require './bi/lib/binary_search/pure'
 
-class Person
-  attr_reader :first_name, :last_name, :orginization
+def stringCompare str1, str2
+	str1.downcase! 
+	pairs1 = (0..str1.length-2).collect {|i| str1[i,2]}.reject {
+	  |pair| pair.include? " "}
+	str2.downcase! 
+	pairs2 = (0..str2.length-2).collect {|i| str2[i,2]}.reject {
+	  |pair| pair.include? " "}
+	union = pairs1.size + pairs2.size 
+	intersection = 0 
 
-  def initialize(first, last, org)
-    @first_name, @last_name, @orginization = first, last, org
-  end
+	pairs1.each do |p1| 
+	  0.upto(pairs2.size-1) do |i| 
+		if p1 == pairs2[i] 
+		  intersection += 1 
+		  pairs2.slice!(i) 
+		  break 
+		end 
+	  end 
+	end 
 
-  def to_s
-    @first_name + " " + @last_name
-  end
+	(2.0 * intersection) / union
 end
 
-class Update
-  attr_reader :type, :content, :person
+class DataMover
+  class Update
+    attr_reader :type, :content, :org_id
 
-  def initialize(type, content, person)
-    @type, @content, @person = type, content, person
-  end
-end
-
-def readFile
-  first = true
-
-  CSV.foreach("contacts.csv") do |row|
-    if first
-      first = false
-      next
-    end
-
-    unless row.last.eql? nil or (row[3].nil? or row[3].empty?) or (row[4].nil? or row[4].empty?)
-      person = Person.new(row[3], row[4], row[6].to_s)
-      puts person
-      handle_updates person, row.last.split(/=== (\w+) ===/)
+    def initialize(type, content, org)
+      @type, @content, @org_id = type, content, org_id
     end
   end
-end
 
-def handle_updates person, updates
-  updates = updates.drop 1 # first item in updates is an empty string
+  def handle_updates org_id, updates
+    updates = updates.drop 1 # first item in updates is an empty string
 
-  while not updates.first.instance_of? Update
-    parts = updates.slice!(0, 2)
-    updates.push Update.new(parts.first, parts.last, person)
+    while not updates.first.instance_of? Update
+      parts = updates.slice!(0, 2)
+      updates.push Update.new(parts.first, parts.last, org_id)
+    end
+
+    updates.each do |update|
+      handle_update update
+    end
   end
 
-  updates.each do |update|
-    handle_update update
+  def handle_update update
+  end
+
+  def find_org_id name
+    r = @orgs.binary_index(name)
+    return @ids[r.last] if r.first
+
+    # Check around correct alphabetical location
+    start = [r.last-2, 0].max
+    finish = [r.last+2, @orgs.size-1].min
+
+    top = [-1, 0]
+    start.upto(finish).each do |i|
+      score = stringCompare @orgs[i].downcase, name.downcase
+      score += 0.3 if @orgs[i].downcase.include? name.downcase \
+                      or name.downcase.include? @orgs[i].downcase
+      top[0], top[1] = i, score if score > top[1]
+    end
+
+    # There were no matches.
+    return nil if top.first.eql? -1
+
+    # There was a 90% or better match.
+    id = top.first
+    return @ids[id] if top.last > 0.9
+
+    # Check the whole list
+    @orgs.each do |o|
+      score = stringCompare o.downcase, name.downcase
+      score += 0.3 if o.downcase.include? name.downcase \
+                      or name.downcase.include? o.downcase
+      top[0], top[1] = o, score if score > top[1]
+      break if top.last > 0.93
+    end
+
+    # There were no good matches.
+    return nil unless top.last > 0.9
+
+    # There was a 90% or better match.
+    id = @orgs.index top.first
+    return @ids[id]
+  end
+
+  def run
+    # open orgs file
+    @ids = []
+    @orgs = []
+    first = true
+    CSV.foreach("orgs.csv") do |row|
+      if first
+        first = false
+        next
+      end
+
+      @ids.push row.first.to_i
+      @orgs.push row[1]
+    end
+    @orgs.sort
+
+    # Open and parse csv data file
+    first = true
+    CSV.foreach("contacts.csv") do |row|
+      if first
+        first = false
+        next
+      end
+
+      unless row.last.eql? nil or (row[6].nil? or row[6].empty?)
+        org_id = find_org_id row[6]
+        handle_updates org_id, row.last.split(/=== (\w+) ===/) unless org_id.nil?
+      end
+    end
   end
 end
 
-def handle_update update
-end
-
-readFile
+DataMover.new().run
